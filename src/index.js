81f4090f41532a8fc10aaa9e0989bbd92ddc2c76
esu <paul.esusen@gmail.com>
2019-04-27 13:02:34 +0200

    add movement

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import { Menu } from './scenes/Menu';
 import { Leo } from './scenes/Leo';
 import { Paul } from './scenes/Paul';
 
+
 const gameConfig = {
   width: 1300,
   height: 700,
   input: {
       gamepad: true
   },
+  physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
   pixelArt: true,
   audio: {
       disableWebAudio: true
@@ -20,4 +27,5 @@ var game = new Phaser.Game(gameConfig);
 game.scene.add('menu', Menu);
 game.scene.add('leo', Leo);
 game.scene.add('paul', Paul);
-game.scene.start("leo", {level: "leo"});
+
+game.scene.start("paul", {level: "paul"});
--- src/scenes/Paul.js
+++ src/scenes/Paul.js
@@ -1,14 +1,52 @@
 export class Paul extends Phaser.Scene {
   constructor () {
-      super({key: 'paul'});
+    super({key: 'paul'});
   }
   preload() {
-    this.load.image('bg', 'assets/placeholder/character.png');
+    this.load.image('bg', 'assets/bg.png');
+    this.load.image('player', 'assets/placeholder/hero.png');
   }
 
   create() {
-    var sprite = this.add.sprite(0, 0, 'bg');
+    console.log("on est chez paul");
+    var sprite = this.add.sprite(300, 300, 'bg');
+    sprite.setScale(8)
+    console.log(this);
+    this.player = this.physics.add.sprite(50, 50, 'player');
   }
   update() {
+    var cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown)
+    {
+      this.player.setVelocityX(-160);
+      //player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+      this.player.setVelocityX(160);
+      //player.anims.play('right', true);
+    }
+    else{
+      this.player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown)
+    {
+      this.player.setVelocityY(-160);
+
+      //player.anims.play('left', true);
+    }
+    else if (cursors.down.isDown)
+    {
+      this.player.setVelocityY(160);
+
+      //player.anims.play('right', true);
+    }
+    else{
+      this.player.setVelocityY(0);
+    }
+
+
+    
   }
 }
