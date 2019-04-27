8b4ba5ea5e6c8722ce8aa13feddc41b6ffb5237c
Léo DESIGAUX <leodesigaux@gmail.com>
2019-04-27 15:10:04 +0200

    Move Player Logic where it sould be dumbass

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,3 +1,4 @@
+import { Coin } from "../entities/Coin";
 export class Player extends Phaser.GameObjects.Sprite {
 
     constructor (scene, x, y)
@@ -14,9 +15,53 @@ export class Player extends Phaser.GameObjects.Sprite {
             'left':4,
             'right':0,
         }
+        this.cooldown = 0;
+        this.shootcooldown = 10;
+        this.accuracy = 10;
+        scene.input.on('pointermove', (pointer) => {
+            pointer.x = pointer.x + scene.cameras.main._scrollX;
+            pointer.y = pointer.y + scene.cameras.main._scrollY; 
+            var angle = Phaser.Math.Angle.BetweenPoints(this, pointer);
+             if((Math.abs(angle) > 0 && Math.abs(angle) < Math.PI/4)) {
+              this.direction = "right";
+            } else if((Math.abs(angle) > 3*Math.PI/4 && Math.abs(angle) < Math.PI)) {
+              this.direction = "left";
+            }else if(angle < 3*Math.PI/4 && angle > Math.PI/4) {
+              this.direction = "down";
+            } else {
+              this.direction = "up";
+            }
+            this._angle = angle;
+        }, this);
+        console.log(this);
     }
     update() {
+        //DIrection 
+        let direction = null
+        if (this.scene   .keys.left.isDown || (this.scene.gamepad && (this.scene.gamepad.left || this.scene.gamepad.axes[0].getValue() < -0.5))) {
+          this.direction = 'left'
+        } else if (this.scene.keys.right.isDown || (this.scene.gamepad && (this.scene.gamepad.right || this.scene.gamepad.axes[0].getValue() > 0.5))) {
+          this.direction = 'right'
+        } else if (this.scene.keys.up.isDown || (this.scene.gamepad && (this.scene.gamepad.up || this.scene.gamepad.axes[1].getValue() < -0.5))) {
+          this.direction = 'up'
+        } else if (this.scene.keys.down.isDown || (this.scene.gamepad && (this.scene.gamepad.down || this.scene.gamepad.axes[1].getValue() > 0.5))) {
+          this.direction = 'down'
+        }
+
+        if(this.cooldown > 0) this.cooldown--;
         this.setFrame(this.atlas[this.direction]);
+
+        if (this.cooldown == 0 && this.scene.keys.space.isDown) {
+            console.log(4);
+          this.cooldown = this.shootcooldown;
+          let coin = new Coin(this.scene, this.body.x,this.body.y);
+          this.scene.coins.push(coin);
+          this.scene.add.existing(coin);
+          this.scene.physics.add.existing(coin);
+          var velocity = new Phaser.Math.Vector2();
+          this.scene.physics.velocityFromRotation(this._angle, 600, velocity);
+          coin .body.setVelocity(velocity.x, velocity.y);
+        }
     }
 
 }
--- src/scenes/Leo.js
+++ src/scenes/Leo.js
@@ -1,5 +1,4 @@
 import { Player } from "../entities/Player";
-import { Coin } from "../entities/Coin";
 
 export class Leo extends Phaser.Scene {
   constructor () {
@@ -30,45 +29,8 @@ export class Leo extends Phaser.Scene {
       restart: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R),
       space: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
     }
-    console.log(this)
-    this.input.on('pointermove', (pointer) => {
-        pointer.x = pointer.x + this.cameras.main._scrollX;
-        pointer.y = pointer.y + this.cameras.main._scrollY; 
-        var angle = Phaser.Math.Angle.BetweenPoints(this.player, pointer);
-         if((Math.abs(angle) > 0 && Math.abs(angle) < Math.PI/4)) {
-          this.player.direction = "right";
-        } else if((Math.abs(angle) > 3*Math.PI/4 && Math.abs(angle) < Math.PI)) {
-          this.player.direction = "left";
-        }else if(angle < 3*Math.PI/4 && angle > Math.PI/4) {
-          this.player.direction = "down";
-        } else {
-          this.player.direction = "up";
-        }
-        this.player._angle = angle;
-    }, this);
   }
   update() {
-    let direction = null
-    if (this.keys.left.isDown || (this.gamepad && (this.gamepad.left || this.gamepad.axes[0].getValue() < -0.5))) {
-      this.player.direction = 'left'
-      this.player._angle += .2;
-    } else if (this.keys.right.isDown || (this.gamepad && (this.gamepad.right || this.gamepad.axes[0].getValue() > 0.5))) {
-      this.player.direction = 'right'
-      this.player._angle -= .2;
-    } else if (this.keys.up.isDown || (this.gamepad && (this.gamepad.up || this.gamepad.axes[1].getValue() < -0.5))) {
-      this.player.direction = 'up'
-    } else if (this.keys.down.isDown || (this.gamepad && (this.gamepad.down || this.gamepad.axes[1].getValue() > 0.5))) {
-      this.player.direction = 'down'
-    }
     this.player.update();
-    if (this.keys.space.isDown) {
-      let coin = new Coin(this, this.player.x,this.player.y);
-      this.coins.push(coin);
-      this.add.existing(coin);
-      this.physics.add.existing(coin);
-      var velocity = new Phaser.Math.Vector2();
-      this.physics.velocityFromRotation(this.player._angle, 600, velocity);
-      coin.body.setVelocity(velocity.x, velocity.y);
-    }
   }
 }
